57db2febb9b977a13025e264e4ab5c279c5e837d
Zak Kemble <zakkemble@gmail.com>
2020-03-26 18:25:46 +0000

    Pass variables to makefile

--- firmware/runmake.bat
+++ firmware/runmake.bat
@@ -1,8 +1,14 @@
-@set MAKE=make.exe
+
+:: Set COMPILER to the location of AVR-GCC bin folder
+:: File path must not contain spaces!
+@set COMPILER=
+
+@set MAKE=%COMPILER%make.exe
+@set AVRDUDE=%COMPILER%avrdude.exe
 
 @cd /d %~dp0
 ::@%MAKE% clean
 @echo CPUs: %NUMBER_OF_PROCESSORS%
-@%MAKE% -j %NUMBER_OF_PROCESSORS%
-@if %errorlevel%==0 %MAKE% upload
+@%MAKE% -j %NUMBER_OF_PROCESSORS% COMPILER=%COMPILER% AVRDUDE=%AVRDUDE%
+@if %errorlevel%==0 %MAKE% upload COMPILER=%COMPILER% AVRDUDE=%AVRDUDE%
 pause
